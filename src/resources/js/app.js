f32a1af8c0c7c99def9e02057d041c269f76a48b
Dmitry Teslitsky <dmitry@teslitsky.info>
2015-02-26 23:04:20 +0200

    Html minor changes

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -5,18 +5,20 @@ Zepto(function ($) {
         if (link.length) {
             $.getJSON('get.php?url=' + encodeURIComponent(link), function (result) {
                 if (result && result.error) {
-                    setError(input);
+                    setError(input, 'Введите корректную ссылку');
                 } else {
                     var audio = result.data;
                     if ($.isArray(audio) && audio.length) {
                         removeError(input);
-                        var template = _.template($("#audioList").html());
-                        $("#audio").html(template({items: audio}));
+                        var template = _.template($('#audioList').html());
+                        $('#audio').html(template({items: audio}));
                     } else {
-                        setError(input);
+                        setError(input, 'Введите корректную ссылку');
                     }
                 }
             });
+        } else {
+            removeError(input);
         }
     }, 800, false));
 
@@ -25,11 +27,13 @@ Zepto(function ($) {
         window.location = 'download.php?' + $(this).data('attachment');
     });
 
-    function setError(input) {
+    function setError(input, error) {
         input.parent().removeClass('has-success').addClass('has-error');
+        $('#result-error').html(error);
     }
 
     function removeError(input) {
         input.parent().removeClass('has-error').addClass('has-success');
+        $('#result-error').empty();
     }
 });

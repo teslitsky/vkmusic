9b13df543262ca8c0d508572178a684ea06fee1f
Dmitry Teslitsky <dmitry@teslitsky.info>
2015-03-03 23:32:01 +0200

    Register MonologServiceProvider

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
 
+$app->register(new Silex\Provider\MonologServiceProvider(), [
+    'monolog.logfile' => __DIR__ . '/../logs/development.log',
+]);
+
 $app->register(new Silex\Provider\TwigServiceProvider(), [
     'twig.path' => __DIR__ . '/../views',
 ]);
